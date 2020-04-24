9bbe737f23b58f03c7c82268e412c01613ff689f
HimDev <brahim-ayad@hotmail.com>
2020-04-24 15:45:18 +0000

    sending information in the url itself

--- store/views.py
+++ store/views.py
@@ -10,3 +10,11 @@ def listing(request):
     message = """<ul>{}
     </ul>""".format("\n".join(albums))
     return HttpResponse(message)
+
+def detail(request, album_id):
+    id = int(album_id) #make sure we have an integer
+
+    album = ALBUMS[id] #get the album with the correspanding id 
+    artists = " ".join([artist['name'] for artist in album['artists']]) # grab artists name and create a string out of it 
+    message = "Le nom de l'album est {}. il a été écrit par {}".format(album['name'], artists)
+    return HttpResponse(message)
